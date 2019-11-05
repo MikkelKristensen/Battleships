82c6a18c953ae8b425edc82cffe8157473e58a4c
MikkelKristensen <nicemikkel@hotmail.com>
2019-11-05 02:08:28 +0100

    Random Stuff

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 #include "Grid.h"
 #include <string>
 
@@ -6,11 +7,37 @@ using namespace std;
 
 int main()
 {
+    int x;
+    int y;
+    int length;
+    string direction;
     Grid * grid = new Grid();
-    // grid->drawGrid();
-    grid->addShip(5, {1,3}, "down");
+
+    /*grid->addShip(5, {1,3}, "down");
     grid->addShip(4, {4,4}, "up");
     grid->addShip(3, {7,4}, "right");
     grid->addShip(2, {10,10}, "up");
-    grid->drawGrid();
+    grid->drawGrid();*/
+
+    // Midlertidigt loop hvor vi bare kan adde skibe og se det opdatere.
+    while(true){
+        system("CLS");
+        grid->drawGrid();
+        cout << endl;
+        cout << "X: ";
+        cin >> x;
+
+        cout << endl;
+        cout << "Y: ";
+        cin >> y;
+
+        cout << endl;
+        cout << "Length: ";
+        cin >> length;
+
+        cout << endl;
+        cout << "direction: ";
+        cin >> direction;
+        grid->addShip(length, {x,y}, direction);
+    }
 }
